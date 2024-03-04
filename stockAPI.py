47a12eb051f458f4c8386195d0b219205f77fb7a
parth <parthkj6@gmail.com>
2024-03-03 19:03:08 -0500

    made Some basic functions

--- stockAPI.py
+++ stockAPI.py
@@ -25,9 +25,12 @@ class Stock:
             'marketCap': i['marketCap'],
         }
         return profile
+    def getHistoricalData(self, start_date: str, end_date: str):
+        data = self.stock.history(start=start_date, end=end_date)
+        return data
     
 c = Stock('AAPL')
 print(c.getCurrentPrice())
 print(c.getCompanyInfo())
-    
+print(c.getHistoricalData('2020-01-01', '2021-01-01'))
 
