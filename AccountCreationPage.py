5f62d996b0febb5809993f3b9c9edffd942ae1c3
Armesh-M <162193317+Armesh-M@users.noreply.github.com>
2024-04-05 22:50:41 -0400

    Add files via upload

--- AccountCreationPage.py
+++ AccountCreationPage.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 import acountManagement
 import InvestmentScreen
+import os
 
 class AccountCreationPageClass():
     def __init__(self, window):
